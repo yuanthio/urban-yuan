faed2eb863418d576e87f55e068855f081c72d51
yuanthio <yuanthiovirly26.9a.tik@gmail.com>
2026-01-03 10:39:42 +0700

    fix new  errors

--- frontend/app/payment/callback/route.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-export function generateMetadata() {
-  return {
-    title: 'Payment Callback',
-    description: 'Payment processing callback page',
-  };
-}
